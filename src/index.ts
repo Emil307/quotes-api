e4b8fc0143b4adb8b64b50cbcaedc9110ee7a0e9
unknown <emilnovikov392@gmail.com>
2023-07-27 02:50:28 +0600

    create express app

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 const express = require('express');
-const dotenv = require('dotenv').config()
-console.log(process.env.PORT);
+const dotenv = require('dotenv').config();
+
+const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+app.listen(PORT, () => {
+    try {
+        console.log(`app started on ${PORT} port`);
+    } catch (e: unknown) {
+        console.log(e);
+    }
+})
