18c904c7f0f1518122a7e6840a0f2bb69198ad60
unknown <emilnovikov392@gmail.com>
2023-07-27 19:18:54 +0600

    connect poet router to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const poetRouter = require('./routes/poet.routes');
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+app.use(express.json());
+app.use('/api', poetRouter)
 
 app.listen(PORT, () => {
     try {
